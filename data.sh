f5bcd9658ba998c8374c15098ff66a54564f99d8
nkunnath <nimal.kunnath@nutanix.com>
2022-04-20 02:20:25 +0200

    Update data insert time interval

--- data.sh
+++ data.sh
@@ -6,7 +6,7 @@ oc exec -it postgresql-prod-0 -- psql "postgresql://postgres:$POSTGRES_PASSWORD@
 #Insert values into Nutanix table
 oc exec -it postgresql-prod-0 -- psql "postgresql://postgres:$POSTGRES_PASSWORD@127.0.0.1/postgres" postgres -c "INSERT INTO nutanix (id, name, location, address, created_on) VALUES (1,'Karbon','USA','San Jose','2009-09-01') ;"
 
-sleep 90
+sleep 180
 
 # Create Redhat table
 oc exec -it postgresql-prod-0 -- psql "postgresql://postgres:$POSTGRES_PASSWORD@127.0.0.1/postgres" postgres -c "CREATE TABLE redhat (id serial PRIMARY KEY, name VARCHAR ( 50 ) UNIQUE NOT NULL, location VARCHAR ( 50 ) NOT NULL, address VARCHAR ( 255 ) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL);"
