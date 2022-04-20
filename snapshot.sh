3d757143c53e86b9d275aea2d17208800e53cc39
nkunnath <nimal.kunnath@nutanix.com>
2022-04-20 02:22:14 +0200

    update snapshot interval to one minute

--- snapshot.sh
+++ snapshot.sh
@@ -6,5 +6,5 @@ for i in {1..5}
      echo "# Snapshot time "`date` >> volumesnapshot.yaml
      oc create -f volumesnapshot.yaml
      sed -i -e "s/postgresql-snapshot-[01234]/postgresql-snapshot-$i/g" volumesnapshot.yaml
-     sleep 30
+     sleep 60
  done
